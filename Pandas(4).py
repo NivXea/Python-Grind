ab5f0e17b0ff30c69c72e8035d7dc02191d21a33
NivXea <akshaysingh1201@gmail.com>
2025-11-16 10:34:41 +0530

    Sorting and Grouping

--- Pandas(4).py
+++ Pandas(4).py
@@ -1,7 +1,8 @@
 #Sorting Data
 
 //Sorting Data in Columns
-Syntax: df.sort_values(by=["Column_name" , etc..] , ascending=[True/False , True/False , etc..] , inplace=True/False) --> Here ascending=True is to sort in ascending order and False to sort in descending order
+Syntax: df.sort_values(by=["Column_name" , etc..] , ascending=[True/False , True/False , etc..] , inplace=True/False) --> Here ascending=True is to sort in ascending order and 
+False to sort in descending order
 
 data = {
   "Name" : [ "Arun" , "Varun" , "Shreya"],
@@ -23,6 +24,34 @@ Output:
 __________________________________________________________________________________________________________________________________________________________________________________________
 #Grouping 
 Syntax: grouped = df.groupby(["column1" , "column2" , ..])
+
+__________________________________________________________________________________________________________________________________________________________________________________________
+#Common Methods
+1. mean()
+2. min()
+3. max()
+4. std() --> standard deviation of values
+5. count() --> count of all none NAN values
+
+__________________________________________________________________________________________________________________________________________________________________________________________
+#Merging and Joining
+Syntax: df_merged = pd.merge(df1 , df2 , on="Column_name" , how="inner/outer/left/right") --> On="Column Name" is the name of common column where u wanna merge
+
+#Customer DF
+df1 = pd.DataFrame({
+    "Customer ID" : [1,2,3,],
+    "Name": ["Akshay" , "Aman" , "Arceus"]
+})
+#Order DF
+df2 = pd.DataFrame({
+    "Customer ID" : [1 , 3 , 5],
+    "Order Amt": [250 , 450 , 440]
+})
+#Merging Both DF's
+df_merged = pd.merge(df1 , df2 , on="Customer ID" , how="outer" )
+
+
+
 
 
 
